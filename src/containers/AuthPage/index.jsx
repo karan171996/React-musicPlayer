22166ffeab8b0e39e36c269bebab3aac26cf3117
Karan Singh <krnsngh38@gmail.com>
2020-12-17 23:02:05 +0530

    Adding Auth Page

--- src/containers/AuthPage/index.jsx
+++ src/containers/AuthPage/index.jsx
@@ -1,22 +1,44 @@
 import React from "react";
 import { Typography, Container } from "@material-ui/core";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import background from "../../assests/background.png";
+import Button from "@material-ui/core/Button";
+import { green } from "@material-ui/core/colors";
 
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: ["Chilanka", "cursive"].join(","),
-    fontSize: 12,
-    fontWeight: 900,
+const useStyles = makeStyles({
+  imageNew: {
+    backgroundImage: `url(${background})`,
+    backgroundPosition: "center",
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    height: "90vh",
+  },
+  buttonStyle: {
+    textAlign: "center",
   },
 });
+
+const AuthButton = withStyles((theme) => ({
+  root: {
+    borderRadius: "20px",
+    color: theme.palette.getContrastText(green[500]),
+    backgroundColor: green[500],
+    "&:hover": {
+      backgroundColor: green[700],
+    },
+  },
+}))(Button);
+
 export default function AuthencationPage() {
+  const classes = useStyles();
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Container fixed>
-          <Typography variant="h2">Hello</Typography>
-        </Container>
-      </ThemeProvider>
-    </>
+    <Container fixed>
+      <Typography className={classes.imageNew} component="div" />
+      <Typography className={classes.buttonStyle} component="div">
+        <AuthButton variant="contained" color="primary">
+          Authenticate Spotify{" "}
+        </AuthButton>
+      </Typography>
+    </Container>
   );
 }
